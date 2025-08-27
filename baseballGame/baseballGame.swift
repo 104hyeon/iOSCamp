cbc73444912ab3d3b594e9cbc73534a538a6a76f
104hyeon <104hyeon@gmail.com>
2025-08-27 16:22:31 +0900

    Lv1. 랜덤 숫자 3개 뽑기

--- baseballGame/baseballGame.swift
+++ baseballGame/baseballGame.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+
+
+class BaseballGame {
+    func start() {
+        let answer = makeAnswer()
+    }
+    
+    func makeAnswer() -> [Int] {
+        var randomNum: [Int] = []
+
+        while randomNum.count < 3 {
+            let number = Int.random(in: 1...9)
+            if !randomNum.contains(number) {   // 중복되는 숫자 제외하기
+                randomNum.append(number)   // 랜덤으로 뽑은 숫자 배열에 추가
+            }
+        }
+        return(randomNum)
+    }
+}
+
+
+
