dd22a65e955652835367866ddc501d268711f064
bakae <bakae@bakaeui-MacBookPro.local>
2025-09-03 17:07:45 +0900

    프로젝트 생성 및 코드베이스로 변경

---
+++ CalculatorApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by bakae on 9/3/25.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow  // viewController 연결 확인 용
+    }
+    
+    
+
+
+}
+
