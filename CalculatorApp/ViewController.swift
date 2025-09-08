122f7b0122ff59a58cdf2998bb0535003f09b7db
104hyeon <104hyeon@gmail.com>
2025-09-08 11:56:13 +0900

    Lv6. 완료  기본 텍스트에 “12345” 가 아닌 “0”으로 수정  기본 텍스트에 버튼을 클릭하면 커튼의 값이 추가되도록 수정  맨 앞자리가 "0"이면 "0"제거하고 숫자 추가

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -1,8 +1,8 @@
 /*
- Lv5.
- 버튼 원형으로 수정하는 단계인데
- Lv2. 요구사항에 `layer.cornerRadius = 40`
- 이미 적용되어 있음
+ Lv6.
+ 기본 텍스트는 “12345” 가 아닌 “0”이 되도록 합니다.
+ 기본으로 라벨에 노출되어있던 텍스트 오른쪽에 버튼을 클릭하면 그 버튼의 값이 추가되도록 합니다.
+ 맨 앞자리가 0 인 숫자라면, 0을 지우고 표현하도록 합니다.
  */
 
 
@@ -10,10 +10,10 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
+    var number: Int = 0
     var resultlabel = UILabel()
-    
     let vStackView = UIStackView()
-
+    
     var sevenButton = UIButton()
     var eightButton = UIButton()
     var nineButton = UIButton()
@@ -51,7 +51,7 @@ class ViewController: UIViewController {
         // resultlabel
         resultlabel.backgroundColor = .black
         resultlabel.textColor = .white
-        resultlabel.text = "12345"
+        resultlabel.text = "\(number)"
         resultlabel.font = .boldSystemFont(ofSize: 60)
         resultlabel.textAlignment = .right
         
@@ -66,34 +66,33 @@ class ViewController: UIViewController {
             .forEach { view.addSubview($0) }
                 
         // 버튼에 타이틀 넣기와 스택 구성하기
-        sevenButton = makeButtons(title: "7", action: nil, backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
-        eightButton = makeButtons(title: "8", action: nil, backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
-        nineButton = makeButtons(title: "9", action: nil, backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
-        plusButton = makeButtons(title: "+", action: nil, backgroundColor: .orange)
+        sevenButton = makeButtons(title: "7", action: #selector(didTapButton), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        eightButton = makeButtons(title: "8", action: #selector(didTapButton), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        nineButton = makeButtons(title: "9", action: #selector(didTapButton), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        plusButton = makeButtons(title: "+", action: #selector(didTapButton), backgroundColor: .orange)
         firstStack = makeHorizontalStackView([sevenButton, eightButton, nineButton, plusButton])
         
-        sixButton = makeButtons(title: "6", action: nil, backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
-        fiveButton = makeButtons(title: "5", action: nil, backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
-        fourButton = makeButtons(title: "4", action: nil, backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
-        minusButton = makeButtons(title: "-", action: nil, backgroundColor: .orange)
+        sixButton = makeButtons(title: "6", action: #selector(didTapButton), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        fiveButton = makeButtons(title: "5", action: #selector(didTapButton), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        fourButton = makeButtons(title: "4", action: #selector(didTapButton), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        minusButton = makeButtons(title: "-", action: #selector(didTapButton), backgroundColor: .orange)
         secondStack = makeHorizontalStackView([sixButton, fiveButton, fourButton, minusButton])
         
-        threeButton = makeButtons(title: "3", action: nil, backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
-        twoButton = makeButtons(title: "2", action: nil, backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
-        oneButton = makeButtons(title: "1", action: nil, backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
-        multiplyButton = makeButtons(title: "*", action: nil, backgroundColor: .orange)
+        threeButton = makeButtons(title: "3", action: #selector(didTapButton), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        twoButton = makeButtons(title: "2", action: #selector(didTapButton), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        oneButton = makeButtons(title: "1", action: #selector(didTapButton), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        multiplyButton = makeButtons(title: "*", action: #selector(didTapButton), backgroundColor: .orange)
         thirdStack = makeHorizontalStackView([threeButton, twoButton, oneButton, multiplyButton])
         
-        acButton = makeButtons(title: "AC", action: nil, backgroundColor: .orange)
-        zeroButton = makeButtons(title: "0", action: nil, backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
-        equalsButton = makeButtons(title: "=", action: nil, backgroundColor: .orange)
-        divideButton = makeButtons(title: "/", action: nil, backgroundColor: .orange)
+        acButton = makeButtons(title: "AC", action: #selector(didTapButton), backgroundColor: .orange)
+        zeroButton = makeButtons(title: "0", action: #selector(didTapButton), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        equalsButton = makeButtons(title: "=", action: #selector(didTapButton), backgroundColor: .orange)
+        divideButton = makeButtons(title: "/", action: #selector(didTapButton), backgroundColor: .orange)
         forthStack = makeHorizontalStackView([acButton, zeroButton, equalsButton, divideButton])
         
         // 가로 스택뷰 vStackView안에 배치
         [firstStack, secondStack, thirdStack, forthStack]
             .forEach { vStackView.addArrangedSubview($0) }
-
     }
     
     
@@ -115,7 +114,7 @@ class ViewController: UIViewController {
     
     // 반복 사용하는 버튼 디자인 함수로 만들기
     // Lv4에서 버튼 컬러 변경으로 함수 수정
-    func makeButtons(title: String, action: Selector?, backgroundColor: UIColor) -> UIButton {
+    func makeButtons(title: String, action: Selector, backgroundColor: UIColor) -> UIButton {
         let button = UIButton()
         button.backgroundColor = backgroundColor
         button.setTitle(title, for: .normal)
@@ -125,6 +124,7 @@ class ViewController: UIViewController {
             $0.width.equalTo(80)
             $0.height.equalTo(80)
         }
+        button.addTarget(self, action: action, for: .touchUpInside)
         return button
     }
     
@@ -140,6 +140,19 @@ class ViewController: UIViewController {
         }
         return stackView
     }
+    
+    @objc
+    func didTapButton(_ sender: UIButton) {
+        
+        if resultlabel.text == "0" {
+            resultlabel.text = " "
+        }
+        resultlabel.text! += sender.currentTitle ?? ""
+
+    }
+
+    
 }
+
 
 
